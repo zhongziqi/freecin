bee2298b17b741f14ce10dc974da6da0c0d45763
agger <850542096@qq.com>
2017-12-03 18:53:00 +0800

    perfect static page

---
+++ js/regist.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+	el: '#main',
+	data: {
+	},
+    methods:{
+        toSend(){
+            
+        }
+    },
+	created: function() {
+	},
+    mounted:function(){
+		
+	}
+
+});
